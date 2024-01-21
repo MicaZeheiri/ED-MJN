f285803f762afb2900a9b522cbca99e75f135cc7
MicaZeheiri <zeheirimica@gmail.com>
2024-01-21 20:20:46 -0300

    Corrección link Mis clases y pagos para deploy

--- js/alumno.js
+++ js/alumno.js
@@ -1,3 +1,10 @@
+document.getElementById('misClasesYPagosLink').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    verificarSesion();
+});
+
+
 function verificarSesion() {
     if (sessionStorage.getItem('usuario')) {
         location.href = '/ED-MJN/pages/alumno.html';
